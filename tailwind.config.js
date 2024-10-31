01a7e8c4cc78bf8beecb7ca5f6875074a076b805
khadiza21 <bibikhadiza474@gmail.com>
2024-10-31 18:43:57 +0545

    banner

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
       },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+};
